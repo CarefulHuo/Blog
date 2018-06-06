7191c8a40c90dab58d1c3f03dc2fb54f6e0a775f
hwy <hnzmdhwy@163.com>
2018-06-06 22:41:59 +0800

    delete raindow

--- bookstore01/src/com/bookStore/admin/user/dao/IAdminUserDao.java
+++
@@ -1,9 +0,0 @@
-package com.bookStore.admin.user.dao;
-
-import com.bookStore.common.beans.User;
-
-public interface IAdminUserDao {
-
-	User selectUserByUNandPW(User user);
-
-}
--- bookstore01/src/com/bookStore/admin/user/service/IAdminUserServcie.java
+++
@@ -1,9 +0,0 @@
-package com.bookStore.admin.user.service;
-
-import com.bookStore.common.beans.User;
-
-public interface IAdminUserServcie {
-
-	User findUserByUNandPW(User user);
-
-}
